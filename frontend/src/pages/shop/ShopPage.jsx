234c562a9efb67dab27c3cd38b1f396a4a8498b1
Daniela Homobono <110701158+danielaHomobono@users.noreply.github.com>
2025-03-13 21:09:47 -0300

    lll

--- frontend/src/pages/shop/ShopPage.jsx
+++ frontend/src/pages/shop/ShopPage.jsx
@@ -1,86 +1,96 @@
-import React, { useEffect } from 'react'
-import productsData from '../../data/products.json'
-import ProductCard from './ProductCard';
-import ShopFiltering from './ShopFiltering';
+import React, { useEffect, useState } from "react";
+import productsData from "../../data/products.json";
+import ProductCard from "./ProductCard";
+import ShopFiltering from "./ShopFiltering";
+import { useFetchAllProductsQuery } from "../../redux/features/products/productsApi";
 
 const filters = {
-    categories: ['all', 'accessories',  'dress', 'jewellery', 'cosmetics'],
-    colors: ['all', 'black',  'red', 'gold', 'blue', 'silver', 'beige', 'green'],
-    priceRanges: [
-        { label: 'Under $50', min: 0, max: 50},
-        { label: '$50 - $100', min: 50, max: 100},
-        { label: '$100 - $200', min: 100, max: 200},
-        { label: '$200 and above', min:200, max: Infinity},
-
-    ]
-}
-
-
+  categories: ["all", "accessories", "dress", "jewellery", "cosmetics"],
+  colors: ["all", "black", "red", "gold", "blue", "silver", "beige", "green"],
+  priceRanges: [
+    { label: "Under $50", min: 0, max: 50 },
+    { label: "$50 - $100", min: 50, max: 100 },
+    { label: "$100 - $200", min: 100, max: 200 },
+    { label: "$200 and above", min: 200, max: Infinity },
+  ],
+};
 
 const ShopPage = () => {
-    const [products, setProducts] = React.useState(productsData);
-    const [filtersState, setFiltersState] = React.useState({
-        category: 'all',
-        color: 'all',
-        priceRange: ''
+  const [filtersState, setFiltersState] = React.useState({
+    category: "all",
+    color: "all",
+    priceRange: "",
+  });
+  const [currentPage, setCurrentPage] = useState(1);
+  const [productsPerPage] = useState(8);
+
+  const { category, color, priceRange } = filtersState;
+  const [minPrice, maxPrice] = priceRange.split("-").map(Number);
+  const {
+    data: { products = [], totalPages, totalProducts } = {},
+    error,
+    isLoading,
+  } = useFetchAllProductsQuery({
+    category: category !== "all" ? category : "",
+    color: color !== "all" ? color : "",
+    minPrice: isNaN(minPrice) ? "" : minPrice,
+    maxPrice: isNaN(maxPrice) ? "" : maxPrice,
+    page: currentPage,
+    limit: productsPerPage,
+  });
+
+  const clearFilters = () => {
+    setFiltersState({
+      category: "all",
+      color: "all",
+      priceRange: "",
     });
-    //Apply filters
-    const applyFilters = () => {
-        let filteredProducts = productsData;
+  };
 
-        if(filtersState.category  && filtersState.category!== 'all'){
-            filteredProducts = filteredProducts.filter(product => product.category === filtersState.category);
-        }
-        if(filtersState.color  && filtersState.color!== 'all'){
-            filteredProducts = filteredProducts.filter(product => product.color === filtersState.color);
-        }
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error.message}</div>;
 
-        
-        if(filtersState.priceRange ){
-            const [minPrice, maxPrice] = filtersState.priceRange.split('-').map(Number);
-            filteredProducts = filteredProducts.filter(product => product.price >= minPrice && product.price <= maxPrice);
-            }
+  const startProduct = (currentPage - 1) * productsPerPage +1;
+  const endProduct = startProduct + products.length -1;
 
-        setProducts(filteredProducts);
-    }
-    useEffect(() => {
-        applyFilters();
-    }, [filtersState])
-
-    const clearFilters = () => {
-        setFiltersState({
-            category: 'all',
-            color: 'all',
-            priceRange: ''
-        })
-    }
-  return (    
+  return (
     <>
-<section className='section__container bg-primary-light'>
-        <h2 className='section__header capitalize'>Shop Page</h2>
-        <p className='section__subheader'>Discover the Hottest Picks: Elevate your Style with Our Curated Collection of Trending Women's Fashion Product
+      <section className="section__container bg-primary-light">
+        <h2 className="section__header capitalize">Shop Page</h2>
+        <p className="section__subheader">
+          Discover the Hottest Picks: Elevate your Style with Our Curated
+          Collection of Trending Women's Fashion Product
         </p>
-    </section>
-    <section className='section__container'>
-        <div className='flex flex-col md:flex-row md:gap-12 gap-8'>
-            {/*Left side*/}
-            <div>
-                <ShopFiltering filters={filters} 
-                filtersState={filtersState}
-                setFiltersState={setFiltersState} 
-                clearFilters={clearFilters}/>
+      </section>
+      <section className="section__container">
+        <div className="flex flex-col md:flex-row md:gap-12 gap-8">
+          {/*Left side*/}
+          <div>
+            <ShopFiltering
+              filters={filters}
+              filtersState={filtersState}
+              setFiltersState={setFiltersState}
+              clearFilters={clearFilters}
+            />
+          </div>
+          {/*Right side*/}
+          <div>
+            <h3 className="text-xl font-medium mb-4">
+              Products Available: {products.length}
+            </h3>
+            <ProductCard products={products} />
+
+            {/*paginations controls*/}
+            <div className="mt-6 flex justify-center">
+               <button className="px-4 py-2 bg-gray-300 text-gray-700 rounded-md mr-2">Previous</button>
+               <button>Next</button>
 
             </div>
-            {/*Right side*/}
-            <div>
-              <h3 className='text-xl font-medium mb-4'>Products Available:{products.length}</h3>
-              <ProductCard products={products}/>
-            </div>
+          </div>
         </div>
-    </section>
-
+      </section>
     </>
-  )
-}
+  );
+};
 
-export default ShopPage
+export default ShopPage;
